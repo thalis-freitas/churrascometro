d834504cbbb80d621a0ae585a7e453acfca56fd6
Thalis Freitas <ofthalis@gmail.com>
2023-02-20 17:37:46 -0300

    refactor: rearrange and clean up

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let meat = 0
 let beer = 0
 let drinks = 0
 let meatForLessTime = 450
-let beerForLessTtime = 1.2
-let drinksForLessTtime = 1
+let beerForLessTime = 1.2
+let drinksForLessTime = 1
 let meatForBiggerTime = 650
 let beerForBiggerTimes = 2
 let drinksForBiggerTime = 1.5
@@ -17,6 +17,8 @@ let settingsArea = document.getElementById('settings-area')
 
 settingsArea.style.display = 'none'
 calculationArea.style.display = 'none'
+document.body.style.background = '#333333'
+document.body.style.color = 'white'
 
 document.querySelector('#settings').addEventListener('click', ()=>{
     settingsArea.style.display = 'inline-block'
@@ -33,17 +35,15 @@ document.querySelector('#go-back').addEventListener('click', ()=>{
     settings.style.display = 'inline-block'
 })
 
-document.querySelector('#to-save').addEventListener('click', ()=>{
-    settings.style.display = 'inline-block'
+let editMinorMeat = document.getElementById('edit-minor-meat')
+let editMinorBeer = document.getElementById('edit-minor-beer')
+let editMinorDrinks = document.getElementById('edit-minor-drinks')
 
-    let editMinorMeat = document.getElementById('edit-minor-meat')
-    let editMinorBeer = document.getElementById('edit-minor-beer')
-    let editMinorDrinks = document.getElementById('edit-minor-drinks')
+let editBiggerMeat = document.getElementById('edit-bigger-meat')
+let editBiggerBeer = document.getElementById('edit-bigger-beer')
+let editBiggerDrinks = document.getElementById('edit-bigger-drinks')
 
-    let editBiggerMeat = document.getElementById('edit-bigger-meat')
-    let editBiggerBeer = document.getElementById('edit-bigger-beer')
-    let editBiggerDrinks = document.getElementById('edit-bigger-drinks')
-
+function validateMeasurements(){
     if (editMinorMeat.value <= 0 ||
         editMinorBeer.value <= 0 ||
         editMinorDrinks.value <= 0 ||
@@ -51,22 +51,27 @@ document.querySelector('#to-save').addEventListener('click', ()=>{
         editBiggerBeer.value <= 0 ||
         editBiggerDrinks.value <= 0){
         alert('Ops... \nPor favor verifique se inseriu as medidas corretamente, o churrascômetro não aceita valores nulos ou negativos.')
+        return false
     }
+}
 
-    else {
-        meatForLessTime = editMinorMeat.value
-        beerForLessTtime = editMinorBeer.value
-        drinksForLessTtime = editMinorDrinks.value
+function changeMeasurements(){
+    meatForLessTime = editMinorMeat.value
+    beerForLessTime = editMinorBeer.value
+    drinksForLessTime = editMinorDrinks.value
+    meatForBiggerTime = editBiggerMeat.value
+    beerForBiggerTime = editBiggerBeer.value
+    drinksForBiggerTime = editBiggerDrinks.value
+    presentationArea.style.display = 'none'
+    calculationArea.style.display = 'inline-block'
+    settingsArea.style.display = 'none'
+    results.style.display = 'inline-block'
+}
 
-        meatForBiggerTime = editBiggerMeat.value
-        beerForBiggerTime = editBiggerBeer.value
-        drinksForBiggerTime = editBiggerDrinks.value
-
-        presentationArea.style.display = 'none'
-        calculationArea.style.display = 'inline-block'
-        settingsArea.style.display = 'none'
-        results.style.display = 'inline-block'
-    }
+document.querySelector('#to-save').addEventListener('click', ()=>{
+    settings.style.display = 'inline-block'
+    if (validateMeasurements() != false)
+        changeMeasurements()
 })
 
 let lightMode = document.getElementById('light-mode')
@@ -93,35 +98,39 @@ document.querySelector('#start').addEventListener('click', ()=>{
 
 function meatWeightFormat() {
     if (meat < 1000) {
-        results.innerHTML = '<h3>' + 'A quantidade ideal para o churrasco é de: ' + '</h3>' + '<p>' + parseInt(meat) + 'g de carne' + '</p>' + '<p>' + beer + 'L de cerveja' + '</p>' + '<p>' + drinks + 'L de refrigerante/suco' + '</p>'
+        results.innerHTML = `<h3> A quantidade ideal para o churrasco é de: </h3> <p> ${parseInt(meat)}g de carne</p><p>${beer}L de cerveja</p><p>${drinks}L de refrigerante/suco</p>`
+    }else{
+        results.innerHTML = `<h3> A quantidade ideal para o churrasco é de: </h3> <p> ${(meat / 1000)}kg de carne</p><p>${beer}L de cerveja</p><p>${drinks}L de refrigerante/suco</p>`
     }
-    if (meat >= 1000)
-        results.innerHTML = '<h3>' + 'A quantidade ideal para o churrasco é de: ' + '</h3>' + '<p>' + (meat / 1000) + 'kg de carne' + '</p>' + '<p>' + beer + 'L de cerveja' + '</p>' + '<p>' + drinks + 'L de refrigerante/suco' + '</p>'
+}
+
+function validateValues(){
+    if (adults.value <= 0 ||
+        children.value < 0 ||
+        duration.value <= 0) {
+        alert('Ops...\nPor favor verifique se inseriu os valores corretamente, é necessário ter no mínimo um adulto e 1 hora de duração.')
+        results.style.display = 'none'
+        return false
+    }
+}
+
+function displayResultForLessTime(){
+    meat = ((adults.value * meatForLessTime) + (children.value * meatForLessTime * 0.5)).toFixed(2)
+    beer = (adults.value * beerForLessTime).toFixed(1)
+    drinks = ((adults.value * drinksForLessTime) + (children.value * drinksForLessTime * 0.5)).toFixed(1)
+    results.style.display = 'inline-block'
+    meatWeightFormat()
+}
+
+function displayResultForBiggerTime(){
+    meat = ((adults.value * meatForBiggerTime) + (children.value * meatForBiggerTime * 0.5)).toFixed(2)
+    beer = (adults.value * beerForBiggerTime).toFixed(1)
+    drinks = ((adults.value * drinksForBiggerTime) + (children.value * drinksForBiggerTime * 0.5)).toFixed(1)
+    results.style.display = 'inline-block'
+    meatWeightFormat()
 }
 
 document.querySelector('#calculate').addEventListener('click', ()=>{
-    if (adults.value <= 0 || children.value < 0 || duration.value <= 0) {
-        alert('Ops...\nPor favor verifique se inseriu os valores corretamente, é necessário ter no mínimo um adulto e 1 hora de duração.')
-
-        results.style.display = 'none'
-    }
-
-    else if (duration.value <= 6 && duration.value > 0) {
-        meat = ((adults.value * meatForLessTime) + (children.value * meatForLessTime * 0.5)).toFixed(2)
-        beer = (adults.value * beerForLessTtime).toFixed(1)
-        drinks = ((adults.value * drinksForLessTtime) + (children.value * drinksForLessTtime * 0.5)).toFixed(1)
-
-        results.style.display = 'inline-block'
-        meatWeightFormat()
-
-    }
-
-    else if (duration.value > 6) {
-        meat = ((adults.value * meatForBiggerTime) + (children.value * meatForBiggerTime * 0.5)).toFixed(2)
-        beer = (adults.value * beerForBiggerTime).toFixed(1)
-        drinks = ((adults.value * drinksForBiggerTime) + (children.value * drinksForBiggerTime * 0.5)).toFixed(1)
-
-        results.style.display = 'inline-block'
-        meatWeightFormat()
-    }
+    if (validateValues() != false)
+        (duration.value <= 6) ? displayResultForLessTime() : displayResultForBiggerTime()
 })
